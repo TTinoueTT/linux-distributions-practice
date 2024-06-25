a64679ba42eed6155b42879eb593fb05273aea1c
t_inoue <t.artwork.ceramica@gmail.com>
2024-06-25 23:38:04 +0900

    First commit

---
+++ kkk.sh
@@ -0,0 +1,20 @@
+[
+  "ユーザ yuko のホームディレクトリに移動する",  
+]
+
+pwd
+/root
+echo $PWD
+/root
+echo ${PWD}
+/root
+
+echo $PS1
+\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w\$
+echo $PS2
+>
+
+echo $PS1
+[\u@\h \W]\$
+echo $PS2
+>
