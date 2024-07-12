67ae74577b41d9241d2a3f927820a8c23d390306
t_inoue <t.artwork.ceramica@gmail.com>
2024-07-13 07:15:32 +0900

    ci(docker): Add docker image for almalinux
    - AlmaLinux が正常に動く Dockerfile を作成
    - docker compose コマンドで起動できるように設定

--- www/test.sh
+++ www/test.sh
@@ -1,50 +1,67 @@
 #!/bin/bash
 
+main_ip=12.13.14.2
 # 元の配列
-validate_ip_count=253
-ip_array=("12.13.14.0/24" "22.23.24.0/22" "32.33.34.0/23" "42.43.44.0/24")
+ip_array=("12.13.14.0/24")
+# ip_array=("12.13.14.0/24" "22.23.24.0/22" "32.33.34.0/23" "42.43.44.0/24")
 
-# 新しい配列の初期化
-ip_addresses=()
-subnet_masks=()
 
-# 関数の定義
+# $1 String - IPv4 アドレスのCIDR形式の文字列が渡される ex. 12.13.14.0/24
 create_ips_from_range () {
-    echo "$1 , $2"
-    # IPアドレスの最後のオクテットをインクリメント
-    for i in {1..$2}
+    # '/'で文字列を分割
+    cidr_ip=$1
+    ip_base="${cidr_ip%/*}"
+    subnet="${cidr_ip##*/}"
+    echo "$ip_base , $subnet"
+
+    # ホスト部の最大数を計算
+    host_bits=$(( 32 - subnet ))
+    host_count=$(( (2**host_bits) - 3 ))  # ネットワークアドレス, ゲートウェイアドレス, ブロードキャストアドレスを除外
+
+    # ネットワークアドレス, ゲートウェイアドレス, ブロードキャストアドレスを計算
+    IFS='.' read -r -a octets <<< "$ip_base"
+    network_address="$ip_base"
+    gateway_address="${octets[0]}.${octets[1]}.${octets[2]}.$((octets[3] + 1))"
+
+    # ブロードキャストアドレスの計算
+    broadcast_octet3=$((octets[2] + (host_count / 256)))
+    broadcast_octet4=$((octets[3] + (host_count % 256) + 2))
+
+    if [ $broadcast_octet4 -gt 255 ]; then
+        broadcast_octet3=$((broadcast_octet3 + 1))
+        broadcast_octet4=$((broadcast_octet4 - 256))
+    fi
+    broadcast_address="${octets[0]}.${octets[1]}.${broadcast_octet3}.${broadcast_octet4}"
+
+    echo "Network Address: $network_address"
+    echo "Gateway Address: $gateway_address"
+    echo "Broadcast Address: $broadcast_address"
+    echo "Host Count: $host_count"
+
+    # IPアドレスの最後のオクテットをインクリメントして出力
+    for i in $(seq 2 "$(( host_count + 1 ))")
     do
-      base_ip="${ip%.*}"
-      new_ip="${base_ip}.${i}"
-      echo "new_ip: ${new_ip}"
+        ip_octet3=$((octets[2] + ((octets[3] + i) / 256)))
+        ip_octet4=$(((octets[3] + i) % 256))
+        new_ip="${octets[0]}.${octets[1]}.${ip_octet3}.${ip_octet4}"
+
+        # main_ip と一致する場合はスキップ
+        if [ "$new_ip" == "$main_ip" ]; then
+            echo "Skipping main_ip: $new_ip"
+            continue
+        fi
+
+        echo "new_ip: ${new_ip}"
     done
 }
 
-# 配列を分割して新しい配列に値を格納
+# create_ips_from_range "22.23.24.0/22"
+
 for ip in "${ip_array[@]}"
 do
-  # '/'で文字列を分割
-  ip_base="${ip%/*}"
-  subnet="${ip##*/}"
-
-  # 新しい配列に値を追加
-  ip_addresses+=("$ip_base")
-  subnet_masks+=("$subnet")
+    echo "================================"
+    create_ips_from_range $ip
+    # echo "$ip"
 done
 
-# 結果を表示
-# echo "ip_addresses: ${ip_addresses[@]}"
-# echo "subnet_masks: ${subnet_masks[@]}"
-
-# 第4オクテットをインクリメントして新しい配列を作成
-new_array=()
-counter=1
-for ip in "${ip_addresses[@]}"
-do
-  # 関数の呼び出し
-  create_ips_from_range ip $validate_ip_count
-  # base_ip="${ip%.*}"
-  # new_ip="${base_ip}.${counter}"
-  # new_array+=("$new_ip")
-  # ((counter++))
-done
+exit
